79646919a96efbf8a71559c0270e99fd74ce2428
Alissar Arnaout <Alissar@Alissars-Air.fc.ca>
2019-09-23 19:32:48 -0400

    test public

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const http = require('http');
+const express = require('express');
+const path = require('path'); // path lets us navigate file/folders
 
-const hostname = '127.0.0.1';
+
+// heroku assigns a port when it deploys via the process (environment variables)
+// locally this will run at port 3000, remotely it will run wherever heroku it is
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {  // js variable for server
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n'); // response
-});
+const app = express();
 
-server.listen(port, hostname, () => { // listen for incoming connections
-  console.log(`Server running at http://${hostname}:${port}/`); 
+app.use(express.static('public')); // looks in public directory
+
+
+app.get('/', (req, res) => {
+  console.log('at the home route');
+  res.sendFile(path.join(__dirname *'/views/index.html'));
+  // this builds localhost:3000/views/index.html
+})
+
+app.get('/contact', (req, res) => {
+  console.log('at the contact route');
+  res.sendFile(path.join(__dirname * '/views/index.html'));
+})
+
+app.get('/porfolio', (req, res) => {
+  console.log('at the portfolio route');
+  res.send('on the portfolio page!');
+})
+
+
+app.listen(port, () => { // listen for incoming connections
+  console.log(`Server running at $(port)`); 
 });
 
 // "node app" in terminal
 // when changes are made --> kill server "^C"
 // we're building a frontend and backend stack
+
+// smh how do u test this stuff??????
+// test heroku
