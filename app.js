7ae13cc9e9efc3b39335e2ae6bccfba81c931788
Alissar Arnaout <Alissar@Alissars-MacBook-Air.local>
2019-09-23 21:20:24 -0400

    working

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
 const express = require('express');
-const path = require('path'); // path lets us navigate file/folders
+const path = require('path'); // path lets us navigate the file system / folders
 
-
-// heroku assigns a port when it deploys via the process (environment variables)
-// locally this will run at port 3000, remotely it will run wherever heroku it is
-
-const port = process.env.PORT || 3000;
+// heroku assigns a port when it deploys via the process (environment variables - coming soon)
+// locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; // a double pipe -> || means "or"
 
 const app = express();
 
-app.use(express.static('public')); // looks in public directory
+app.use(express.static('public'));
 
 
+// a forward slash is the home route (same as index.html)
 app.get('/', (req, res) => {
   console.log('at the home route');
-  res.sendFile(path.join(__dirname *'/views/index.html'));
+  res.sendFile(path.join(__dirname + '/views/index.html'));
   // this builds localhost:3000/views/index.html
 })
 
 app.get('/contact', (req, res) => {
   console.log('at the contact route');
-  res.sendFile(path.join(__dirname * '/views/index.html'));
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
 })
 
-app.get('/porfolio', (req, res) => {
+app.get('/portfolio', (req, res) => {
   console.log('at the portfolio route');
   res.send('on the portfolio page!');
 })
 
 
-app.listen(port, () => { // listen for incoming connections
-  console.log(`Server running at $(port)`); 
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
-
-// "node app" in terminal
-// when changes are made --> kill server "^C"
-// we're building a frontend and backend stack
-
-// smh how do u test this stuff??????
-// test heroku
